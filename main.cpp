a2d3da3511c38800a0c44658358ca94b4fff5ed2
Hasnat M <61457702+hasnat-hybrid@users.noreply.github.com>
2021-07-11 14:59:47 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+int adjacentElementsProduct(vector<int> inputArray) {
+
+    		int product=inputArray.at(0)*inputArray[1];
+	for (int i=1;i<inputArray.size()-1;i++){
+		int productTemp=inputArray.at(i)*inputArray[i+1];
+		if(product<productTemp){
+			product=productTemp;
+		}
+
+	}
+
+	return product;
+}
